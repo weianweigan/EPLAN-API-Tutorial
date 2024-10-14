4e31b0c862e56efe028eb627151fa9dfc5519d75
dududu <weianweigan@gmail.com>
2024-10-14 17:48:48 +0800

    add debug info

---
+++ EPLANAPI.EplAddin.SampleAddin/AddInModule.cs
@@ -0,0 +1,47 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.EServices;
+using System.Diagnostics;
+using System.Windows;
+
+namespace EPLANAPI.EplAddin.SampleAddin;
+
+public class AddInModule : IEplAddIn
+{
+    public bool OnExit()
+    {
+        return true;
+    }
+
+    public bool OnInit()
+    {
+        return true;
+    }
+
+    public bool OnInitGui()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// 文件->附加->扩展->接口-> 加载
+    /// </summary>
+    /// <param name="bLoadOnStart"></param>
+    /// <returns></returns>
+    public bool OnRegister(ref bool bLoadOnStart)
+    {
+        Debugger.Break();
+        string path = typeof(AddInModule).Assembly.Location;
+        MessageBox.Show(path);
+        bLoadOnStart = true;
+        return true;
+    }
+
+    /// <summary>
+    /// 文件->附加->扩展->接口-> 卸载
+    /// </summary>
+    /// <returns></returns>
+    public bool OnUnregister()
+    {
+        return true;
+    }
+}
---
+++ Tutorials/How to debug eplan with visual studio.md
@@ -0,0 +1,9 @@
+﻿# 如何使用Visual Studio 调试EPLAN
+
+## 一. 使用vs附加调试
+
+| EPLAN使用Themida保护了源代码导致无法使用Visual Studio调试。使用Visual Studio 启动Eplan.exe可以得到此错误。直接附加进程将会导致Eplan重启。
+
+![debugger error](debugger.png)
+
+
